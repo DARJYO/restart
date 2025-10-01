0265d8362104ee0a508473660118b8096e2341e3
persadian <34051185+arishma108@users.noreply.github.com>
2025-10-01 18:55:41 +0200

    Add files via upload

---
+++ components/icons/TargetIcon.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+export const TargetIcon: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={2}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15 15l-2 5L9 9l7-7-5 2zM9 9l-2 5L3 3l7 4-2-2z"
+    />
+  </svg>
+);
